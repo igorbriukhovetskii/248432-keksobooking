397bf9ddbe0992e16a1fa7740d5fca03d91b3dcb
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-12-04 14:37:57 +0300

    Добавляет функцию получения случайного числа в заданном диапазоне

--- js/map.js
+++ js/map.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/**
+ * Получение случайного целого числа в заданном диапазоне
+ * @param {number} maxValue - максимальное возможное число
+ * @param {number} minValue - минимальное возможное число, по умолчанию равно 0
+ * @return {number} - случайное целое число
+ */
+var getRandomInteger = function (maxValue, minValue) {
+  minValue = minValue || 0;
+  return Math.floor(Math.random() * (maxValue + 1 - minValue) + minValue);
+};
